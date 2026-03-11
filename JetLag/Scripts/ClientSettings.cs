5103d2815dfe925b17d7d890bc58d611c6a6228a
JGough98 <94226526+JGough98@users.noreply.github.com>
2026-03-11 19:32:13 +0200

    Minor refactor
    Swapped over to referencing the new player class in the Client settings.

---
+++ JetLag/Scripts/ClientSettings.cs
@@ -0,0 +1,22 @@
+namespace JetLag.Scripts;
+
+
+/// <summary>
+/// Manages the state of the player.
+/// </summary>
+public class ClientSettings
+{
+    private Player? _player;
+
+
+    public Player Player
+    {
+        get => _player;
+    }
+
+    public string PlayerName
+    {
+        get => _player.Name;
+        set => _player = new Player(StringUtility.Sanitise(value), true);
+    }
+}
